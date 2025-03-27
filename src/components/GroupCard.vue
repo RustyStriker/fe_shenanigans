b22e814455592387d1b50d504995c8fefbf59ec1
Rusty Striker <avivr903@tutanota.com>
2025-03-27 17:35:39 +0200

    aaaaaabbbbbbc

--- src/components/GroupCard.vue
+++ src/components/GroupCard.vue
@@ -10,10 +10,11 @@ export default {
 </script>
 <template>
     <div class="glowHover"
-        style="display: flex; flex-direction: column; background-color: #9090fc; padding: 16px; border-radius: 8px; margin: 8px;">
+        style="display: flex; flex-direction: column; background-color: #9090fc; padding: 0px; border-radius: 8px; margin: 8px;">
         <h3 style="align-self: center;">{{ title }}</h3>
         <img :src="imageUrl" width="512">
-        <div style="display: flex;">
+        <div style="flex-grow: 1"></div>
+        <div style="display: flex; padding: 0px 8px;">
             <p style="flex-grow: 1;">{{ cost }}</p>
             <p>{{ date }}</p>
         </div>
@@ -26,6 +27,6 @@ export default {
 }
 
 .glowHover:hover {
-    box-shadow: 4px 4px 16px blue;
+    box-shadow: 4px 4px 8px blue;
 }
 </style>
